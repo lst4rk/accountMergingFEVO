6a8861a4656b58a914a651af8dec8ec36e407b84
lst4rk <olivialistokin@gmail.com>
2022-10-19 21:38:30 -0400

    set up environment

---
+++ index.js
@@ -0,0 +1,12 @@
+// imports json data from accounts.json
+const accountsData = require('./accounts.json');
+
+// initial check for correctly imported data
+console.log(accountsData);
+
+// To Do: create listing  of people from data
+
+// find identical emails
+    // merge emails -> [string]
+    // merge applications -> [string]
+    // if names differ, pick one -> string
