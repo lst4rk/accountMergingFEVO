4483d4604cbf0fe236b6ed484b61d1841d2bb208
lst4rk <olivialistokin@gmail.com>
2022-10-21 15:13:57 -0400

    refactored code for readability, adjusted variable names for specificity

--- index.js
+++ index.js
@@ -1,77 +1,104 @@
-// imports json data from accounts.json
+// import JSON data from accounts.json
 const accountsData = require('./accounts.json');
 
-
-
-function mergeAccounts(accounts) {
-    // adds bool property for use later to keep track of processing
+function mergeAccounts(accounts){
+    // initialize empty array to console.log after complete merge event
+    let peopleArr = [];
+    // add false bool on objects for tracking processing
     accounts.forEach(account => account.isHandled = false);
+    // iterate through objects for non-processed accounts (isHandled = false)
+    findNonHandledAcc(accounts, peopleArr);
+    // return JSON structure as specified in instructions of instructions.txt 
+    // console.log(JSON.stringify(peopleArr, undefined, 2));
+    // return same structure as in instructions.txt example output
+    console.log(peopleArr);
+}
 
-    // initializes empty array to return after complete merge event
-    let mergedAccountsArr = [];
+function isHandled(account) {
+    account.isHandled = true;
+}
 
-    // iterates through imported JSON data
-    accounts.forEach((acc, accIndex, accArr) => {
-        // checks if current account has been marked as handled and moves onto next iteration if so
-        if (acc.isHandled) return;
-
-        // if current account is not marked as handled, new object is created
-        let newAccObj = {
-            // ensures elements are added to the application array as strings
-            applications: [String(acc.application)],
-            emails: acc.emails,
-            name: acc.name,
-        };
-
-        // marks current account as handled
-        acc.isHandled = true;
-
-
-        // loops through all of the emails ([acc.emails]) within the current account (acc)
-        let emailArr = [...acc.emails]
-
-        while (emailArr.length > 0) {
-            // grabs first email in array
-
-            let email = emailArr.shift();            
-
-            // filters through all acounts to get array of those that have both 1) not been previously handled and 2) whose emails include the current email in the iteration
-            let acctsToMerge = accArr.filter(acc => !acc.isHandled && acc.emails.includes(email));
-            acctsToMerge.forEach((mergeAcc, i) => {
-
-                if (mergeAcc.isHandled) return;
-
-                // application property: maintains string data type if single value, converts to array if multiple applications. pushes # and sorts array numerically
-                newAccObj.applications.push(String(mergeAcc.application))
-                if (newAccObj.applications.length >= 1) {
-                    newAccObj.applications.sort((a,b) => a-b)
-                }
-
-                // emails property: Set constructor used to avoid adding duplicates to an account
-                    newAccObj.emails.push(...mergeAcc.emails)
-                    newAccObj.emails = [... new Set(newAccObj.emails)].sort();
-
-                // adds other emails connected to same person but on a separare account
-                mergeAcc.emails.forEach(email => emailArr.push(email));
-                
-
-                // marks account as handled
-                mergeAcc.isHandled = true;
-            });
-
-        }
-        // pushes account obj to returning array
-        mergedAccountsArr.push((newAccObj));
-
+function findNonHandledAcc(accounts, peopleArr){
+    accounts.forEach((account, accIndex, accArr) => {
+        // check if current account has not been handled, create new obj for the associated person
+        if (account.isHandled) return;
+        createNewPerson(account, accArr, peopleArr);
     })
-
-    // returns same structure as in instructions.txt example output:
-    // return mergedAccountsArr
-
-    // returns JSON structure
-    return JSON.stringify(mergedAccountsArr, undefined, 2);
 
 }
 
+function createNewPerson(account, accArr, peopleArr){
+    let newPersonObj = {
+        applications: [String(account.application)],
+        emails: account.emails,
+        name: account.name,
+    };
+    // mark account as now handled
+    isHandled(account);
 
-console.log(mergeAccounts(accountsData))
+    // loop through emails within current account
+    matchEmails(newPersonObj, accArr, peopleArr);
+}
+
+function matchEmails(newPersonObj, accArr, peopleArr){
+    // add all emails within current account to an array as ref for matches
+    let emailArr = [...newPersonObj.emails];
+
+    // find all matching emails in unprocessed accts
+    compareAllEmails(emailArr, newPersonObj, accArr);
+    // after data is merged to newPersonObj, push new person to peopleArray
+    addNewPerson(newPersonObj, peopleArr);
+}
+
+function compareAllEmails(emailArr, newPersonObj, accArr){
+    while (emailArr.length > 0){
+        let email = emailArr.shift();  
+          
+        // for every email in ref account, create array of accounts that haven't been processed and have matching emails to ref email
+        findMatchingAndUnhandled(email, emailArr, newPersonObj, accArr);
+    }
+}
+
+function findMatchingAndUnhandled(email, emailArr, newPersonObj, accArr){
+    let acctsToMerge = accArr.filter(acc => !acc.isHandled && acc.emails.includes(email));
+
+    // using array of matching accounts, combine application and email data to newPersonObj
+    combineAccountData(emailArr, newPersonObj, acctsToMerge);
+}
+
+function combineAccountData(emailArr, newPersonObj, acctsToMerge){
+    acctsToMerge.forEach((accToMerge) => {
+        // if matching account has not been handled, combine emails and application #s
+        if (accToMerge.isHandled) return;
+
+        mergeApplications(newPersonObj, accToMerge);
+        mergeEmails(emailArr, newPersonObj, accToMerge);
+
+        // mark accounts merged as handled
+        isHandled(accToMerge);
+    })
+}
+
+function mergeApplications(newPersonObj, accToMerge){
+    // add application # and put in increasing order
+    newPersonObj.applications.push(String(accToMerge.application))
+    if (newPersonObj.applications.length >= 1) {
+        newPersonObj.applications.sort((a,b) => a-b)
+    }
+}
+
+function mergeEmails(emailArr, newPersonObj, accToMerge){
+    // add unique emails and alphabetize
+    newPersonObj.emails.push(...accToMerge.emails)
+    newPersonObj.emails = [... new Set(newPersonObj.emails)].sort();
+
+    // add all new emails associated with newPersonObj to reference array
+    accToMerge.emails.forEach(email => emailArr.push(email));
+}
+
+function addNewPerson(newPersonObj, peopleArr) {
+    // add new fully merged and unique person to array of people objects
+    peopleArr.push((newPersonObj));
+}
+
+mergeAccounts(accountsData)
